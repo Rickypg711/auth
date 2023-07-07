479f49a046d4f885c2ce938802fdf411da7724d2
Rickypg711 <77697094+Rickypg711@users.noreply.github.com>
2023-07-07 13:23:14 -0700

    authshit

---
+++ app/api/auth/[...nextauth]/options.tsx
@@ -0,0 +1,46 @@
+import type { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+
+export const options: NextAuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username:",
+          type: "text",
+          placeholder: "yourname",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "pass",
+        },
+      },
+      async authorize(credentials) {
+        const user = { id: "42", name: "Ricky", password: "nextauth" };
+
+        if (
+          credentials?.username === user.name &&
+          credentials?.password === user.password
+        ) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+};
